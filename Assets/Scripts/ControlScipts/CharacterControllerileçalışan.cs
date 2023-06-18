5d3252de737bed98e64aadcea39d0eb37e508cf0
FtuncS <furkantunc218@gmail.com>
2023-06-18 12:46:22 +0300

    animation sc
    animasyon için sc güncelledim animator controller ekledim ama bir yerlerde bug oluyor asla düzgün kodu yazamadım sorunu bulamadım videolardakinin aynılarını yapsam bile aynı hatalar devam etti

--- Assets/Scripts/ControlScipts/CharacterControllerileçalışan.cs
+++ Assets/Scripts/ControlScipts/CharacterControllerileçalışan.cs
@@ -11,10 +11,12 @@ public class CharacterMovement : MonoBehaviour
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityÇarpanı;
+    private Animator animator;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,21 +25,31 @@ public class CharacterMovement : MonoBehaviour
         float moveZ = Input.GetAxis("Vertical"); // Dikey hareket için Input
 
         // Hareket yönünü hesapla
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical")* moveSpeed);
-        
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
 
         // Zıplama kontrolü
         if (controller.isGrounded)
         {
+
             if (Input.GetButtonDown("Jump"))
             {
+                animator.SetBool("jumpbool",true);
+                animator.SetBool("grounded", false);
                 moveDirection.y = jumpForce;
+                
+                
             }
         }
 
+        
 
         // Hareket uygula
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityÇarpanı * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
+
+        animator.SetFloat("speed", Mathf.Abs(controller.velocity.x) + Mathf.Abs(controller.velocity.z));
+        
+
     }
 }
